86080019d0b6f6c899dfb4d97c28354cf74119be
nmy2103 <natasha.yamane@gmail.com>
2025-09-15 17:02:41 -0400

    Update package imports

--- physioview/__init__.py
+++ physioview/__init__.py
@@ -1,57 +1,36 @@
 # __init.py
 __version__ = '1.0'
 
-# Import convenient modules and classes
-from physioview import physioview
-from physioview.pipeline.SQA import Cardio, EDA
-from physioview.pipeline.ECG import Filters as ecg_filters
-from physioview.pipeline.ECG import BeatDetectors as ecg_beat_detectors
-from physioview.pipeline.PPG import Filters as ppg_filters
-from physioview.pipeline.PPG import BeatDetectors as ppg_beat_detectors
+# Import signal preprocessing and quality assessment modules and classes
+from physioview.pipeline.SQA import Cardio as CardioQA, EDA as EDAQA
+from physioview.pipeline.ECG import Filters as ECGFilters, BeatDetectors as ECGBeatDetectors
+from physioview.pipeline.PPG import Filters as PPGFilters, BeatDetectors as PPGBeatDetectors
+from physioview.pipeline.EDA import Filters as EDAFilters
+from physioview.pipeline import ECG, PPG, EDA, SQA
 
-# Initialize SQA classes
-def cardio_sqa(fs: int) -> 'Cardio':
-    """Initialize the physioview.pipeline.SQA.Cardio class containing signal
-    quality assessment functions for ECG and PPG data."""
-    return Cardio(fs)
+# Import public symbols from physioview.py
+from physioview.physioview import *
+import physioview.physioview as _physioview
 
-def eda_sqa(
-    fs,
-    eda_min: float = 0.2,
-    eda_max: float = 40,
-    eda_max_slope: float = 5,
-    temp_min: float = 20,
-    temp_max: float = 40,
-    invalid_spread_dur: float = 2.5
-) -> 'EDA':
-    """Initialize the physioview.pipeline.SQA.EDA class containing signal
-    quality assessment functions for EDA data.
+# Create alias for `EDA.compute_features()`
+def compute_eda_features(signal, fs, window_size, step_size):
+    return EDA.compute_features(signal, fs, window_size, step_size)
 
-    Parameters
-    ----------
-    fs : int
-        The sampling rate of the EDA recording.
-    eda_min : float, optional
-        The minimum acceptable value for EDA data in microsiemens; by
-        default, 0.2 uS.
-    eda_max : float, optional
-        The maximum acceptable value for EDA data in microsiemens; by
-        default, 40 uS.
-    eda_max_slope : float, optional
-        The maximum slope of EDA data in microsiemens per second; by
-        default, 5 uS/sec.
-    temp_min : float, optional
-        The minimum acceptable temperature in degrees Celsius; by
-        default, 20.
-    temp_max : float, optional
-        The maximum acceptable temperature in degrees Celsius; by
-        default, 40.
-    invalid_spread_dur : float, optional
-        The transition radius for artifacts in seconds; by default,
-        2.5 seconds.
-"""
-    return EDA(fs)
+# Expose classes and methods
+__all__ = [
+    'CardioQA',
+    'EDAQA',
+    'ECGFilters',
+    'ECGBeatDetectors',
+    'PPGFilters',
+    'PPGBeatDetectors',
+    'EDAFilters',
+    'ECG',
+    'PPG',
+    'EDA',
+    'SQA',
+    'compute_eda_features',
+]
 
-# Expose functions
-__all__ = ['cardio_sqa', 'ecg_filters', 'ecg_beat_detectors',
-           'ppg_filters', 'ppg_beat_detectors']
+# Extend __all__ with everything public from physioview.py
+__all__ += _physioview.__all__
