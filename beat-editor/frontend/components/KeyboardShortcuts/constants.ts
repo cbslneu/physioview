a6cd613543119ccc0967533a417462ebf54a0382
Tim Chen <tchen994@gmail.com>
2026-06-29 12:23:16 -0400

    Update keyboard shortcuts

---
+++ beat-editor/frontend/components/KeyboardShortcuts/constants.ts
@@ -0,0 +1,41 @@
+export const COMMON_CLASSNAMES = {
+  shortcutItem: "shortcut-item",
+  shortcutKeys: "shortcut-keys",
+  shortcutLabel: "shortcut-label",
+  markUnusableOption: "mark-unusable-option",
+  keybind: "keybind",
+};
+
+export const TOP_ROW_SHORTCUTS = [
+  {
+    icon: "fa-solid fa-a keybind",
+    label: "Add Beat",
+  },
+  {
+    icon: "fa-solid fa-d keybind",
+    label: "Delete Beat",
+  },
+  {
+    icon: "fa-solid fa-r keybind",
+    label: "Remove",
+  },
+];
+
+export const MIDDLE_ROW_SHORTCUTS = [
+  {
+    uIcon: "fa-solid fa-u keybind",
+    plusIcon: "fa-solid fa-plus plus",
+    rightClickIcon: "../../public/images/right-click.png",
+    rightClickKeybind: "rt-click-keybind",
+    markUnusableLabel: "Mark Unusable",
+  },
+];
+
+export const BOTTOM_ROW_SHORTCUTS = [
+  {
+    shiftLabel: "SHIFT",
+    pointerIcon: "fa-solid fa-arrow-pointer keybind",
+    dragLabel: "DRAG",
+    panLabel: "PAN",
+  },
+];
